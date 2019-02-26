5fb2beb5b5b451904cfba6bb7923f79c5a7f82b9
Robert Klotzner <robert.klotzner@gmx.at>
2019-02-26 20:15:41 +0100

    Added missing error pretty printing.

--- common/Obelisk/OAuth/Error.hs
+++ common/Obelisk/OAuth/Error.hs
@@ -37,4 +37,9 @@ data OAuthError
 
 textOAuthError :: OAuthError -> Text
 textOAuthError = ("ERROR: " <>) .  \case
-  OAuthError_MissingCodeState -> "URI was missing required code or state parameters!"
+  OAuthError_MissingCodeState
+    -> "URI was missing required code or state parameters!"
+  OAuthError_InvalidState
+    -> "We got a statate from the authorization provider that did not match the one we sent."
+  OAuthError_NoSessionState
+    -> "We received a redirect from the authorization provider, but found no state to match too in session storage."
