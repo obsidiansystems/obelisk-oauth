38c778a0bd47810cd82981d0059658fec0a63100
Ali Abrar <aliabrar@gmail.com>
2021-04-01 20:36:40 -0400

    Add auth request type to support draft-ietf-oauth-v2-07
    See https://tools.ietf.org/id/draft-ietf-oauth-v2-07.html#user_authorization

--- backend/Obelisk/OAuth/AccessToken.hs
+++ backend/Obelisk/OAuth/AccessToken.hs
@@ -22,6 +22,7 @@ data TokenRequest r = TokenRequest
   , _tokenRequest_clientId :: Text
   , _tokenRequest_clientSecret :: Text
   , _tokenRequest_redirectUri :: (R OAuth -> R r)
+  , _tokenRequest_type :: Maybe AuthorizationRequestType
   }
 
 getOauthToken
@@ -32,12 +33,8 @@ getOauthToken
   -> IO Request
 getOauthToken reqUrl appRoute enc t = do
   req <- parseRequest reqUrl
-  let form =
-        [ partBS "client_id" $ T.encodeUtf8 $ _tokenRequest_clientId t
-        , partBS "client_secret" $ T.encodeUtf8 $ _tokenRequest_clientSecret t
-        , partBS "redirect_uri" $ T.encodeUtf8 $
-            appRoute <> renderBackendRoute enc (_tokenRequest_redirectUri t $ OAuth_RedirectUri :/ Nothing)
-        ] ++ case _tokenRequest_grant t of
+  let grant = case _tokenRequest_type t of
+        Nothing -> case _tokenRequest_grant t of
           TokenGrant_AuthorizationCode code ->
             [ partBS "grant_type" "authorization_code"
             , partBS "code" code
@@ -46,4 +43,19 @@ getOauthToken reqUrl appRoute enc t = do
             [ partBS "grant_type" "refresh_token"
             , partBS "refresh_token" refresh
             ]
+        Just rt -> 
+          [ partBS "type" $ authorizationRequestTypeToText rt
+          ] <> case _tokenRequest_grant t of
+          TokenGrant_AuthorizationCode code ->
+            [ partBS "code" code
+            ]
+          TokenGrant_RefreshToken refresh ->
+            [ partBS "refresh_token" refresh
+            ]
+      form =
+        [ partBS "client_id" $ T.encodeUtf8 $ _tokenRequest_clientId t
+        , partBS "client_secret" $ T.encodeUtf8 $ _tokenRequest_clientSecret t
+        , partBS "redirect_uri" $ T.encodeUtf8 $
+            appRoute <> renderBackendRoute enc (_tokenRequest_redirectUri t $ OAuth_RedirectUri :/ Nothing)
+        ] <> grant
   formDataBody form $ req { method = "POST" }
